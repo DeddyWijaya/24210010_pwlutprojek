f678f04235774c9c2f5f5ee14a27e902bb124bc9
DeddyWijaya <deddywijaya@itbss.ac.id>
2026-04-27 21:35:39 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('create');
+});
+
+Route::resource('mahasiswa', MahasiswaController::class);
+
